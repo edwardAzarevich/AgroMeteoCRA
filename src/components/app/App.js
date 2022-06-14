82f4957372deb9a55680900692056df2d8df2bf5
EdwardAzarevich <Edward.azarevich@gmail.com>
2022-06-14 17:02:42 +0300

    add MainHeader

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MainHeader from './header/header'
+
+function App() {
+    return (
+        <div className="App">
+            <MainHeader />
+            <header className="App-header">
+                <h1>Hello</h1>
+            </header>
+        </div>
+    );
+}
+
+
+
+export default App;
---
+++ src/components/app/header/header.js
@@ -0,0 +1,14 @@
+import './header.css';
+
+function MainHeader() {
+    return (
+        <div className="App">
+            <header>
+                <h1>Пеленг</h1>
+                <nav><a>First Link</a><a>Second Link</a><a>Third Link</a></nav>
+            </header>
+        </div>
+    );
+}
+
+export default MainHeader;
