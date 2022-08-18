e6edae6835814983a7db1e384fbfa8e5677f3fbb
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2022-08-18 17:08:34 +0300

    add header, scss

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1>asdfsadfsad</h1>
+            <h1></h1>
         </div>
     )
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,20 @@
-import './header.css';
+//import './header.css';
+import './header.scss';
 
 function MainHeader() {
     return (
         <div >
             <header>
-                <div className='MainHeader'>
-                    <div>Peleng</div>
-                    <nav><a>First Link</a><a>Second Link</a><a>Third Link</a></nav>
-                </div>
+
+                <p class="logo">Peleng</p>
+                <nav>
+                    <ul class="nav_links">
+                        <li><a href="#">Telegram</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </nav>
+                <a class="cta" href="#"><button>Log In</button></a>
             </header>
         </div>
     );
